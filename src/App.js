94a923617411b6393c6b099ea8be0431cd7d7e04
SwippyA <shubhnkarswain16@gmail.com>
2024-01-17 18:29:58 +0530

    sdgf

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import  express from "express";
+import {cors} from "cors"
+import cookieParser from 'cookie-parser'
+
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_CONNECT,
+    Credentials:true
+}))
+app.use(express.json({limit:"16KB"}))
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16KB"
+}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export  default app ;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import Connection_db from './Database/index.js'
-import dotenv from 'dotenv'
+import app from "./App.js";
+import Connection_db from "./Database/index.js";
+import dotenv from "dotenv";
 dotenv.config({
-    path:"./env"
-})
+  path: "./env",
+});
+
 Connection_db()
+  .then(() => {
+    app.listen(process.env.Port || 8000, () => {
+      console.log(`the server is serve at Port ${process.env.Port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
