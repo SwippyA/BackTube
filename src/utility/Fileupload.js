c6ca980c89dbc0ff0a4f8345a648d60e0ea21d72
SwippyA <shubhnkarswain16@gmail.com>
2024-01-24 08:37:10 +0530

    Cloudnary is done

---
+++ src/utility/Fileupload.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary'
+import { log } from 'console';
+import fs from "fs"
+
+// import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret 
+});
+
+const UploadFile = async(LocalFilepath) =>{
+    try{
+        if(!LocalFilepath) return null;
+        const Upload = await cloudinary.uploader.upload(LocalFilepath,{
+            resource_type:'auto'
+        })
+        console.log("File have been uploaded :",Upload.url);
+        return Upload;
+    }
+    catch{
+        fs.unlinkSync(LocalFilepath);
+        return null;
+    }
+}
+
+export {UploadFile};
