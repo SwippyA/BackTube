3540d22c68b6bc51ec9fd786dece8c1abbc184b3
SwippyA <shubhnkarswain16@gmail.com>
2024-02-10 17:44:43 +0530

    dfds

--- src/Route/User.js
+++ src/Route/User.js
@@ -1,26 +1,47 @@
 import { Router } from "express";
-import { registerUser ,LoginUser, Logout,RefreshToken} from "../Controller/User.js";
-import { upload } from '../Middleway/Multer.js'
-import {verifyJWT} from "../Middleway/Verify.js"
+import {
+  registerUser,
+  LoginUser,
+  Logout,
+  RefreshToken,
+  changePassword,
+  updateUserCoverImage,
+  updateUserAvatar,
+  updateAccountDetails,
+  getCurrentUser,
+  userProfile,
+  getWatchHistory,
+} from "../Controller/User.js";
+import { upload } from "../Middleway/Multer.js";
+import { verifyJWT } from "../Middleway/Verify.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-    )
-    router.route("/login").post(LoginUser)
-    router.route("/logout").post( verifyJWT , Logout) 
-    router.route("/refresh-token").post( RefreshToken ) 
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(LoginUser);
+router.route("/logout").post(verifyJWT, Logout);
+router.route("/refresh-token").post(RefreshToken);
 
+router.route("/change-password").post(verifyJWT, changePassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, userProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
