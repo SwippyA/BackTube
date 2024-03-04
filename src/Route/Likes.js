d9c9a3d0fecac39a367741d702228db6c4511849
SwippyA <shubhnkarswain16@gmail.com>
2024-03-04 18:30:42 +0530

    comment likes is also done

--- src/Route/Likes.js
+++ src/Route/Likes.js
@@ -11,7 +11,7 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
-router.route("/").post(toggleCommentLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
 router.route("/toggle/t/:tweetId").post(toggleTweetLike);
 router.route("/videos").get(getLikedVideos);
 
