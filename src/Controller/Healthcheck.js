d2e73868d8150bdb1e844898704f40d85d82b104
SwippyA <shubhnkarswain16@gmail.com>
2024-03-10 21:52:40 +0530

    health check is also done

--- src/Controller/Healthcheck.js
+++ src/Controller/Healthcheck.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utility/ApiRespone.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
   //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  return res.status(200).json(new ApiResponse(200,[],"the app is running without any error"))
 });
 
 export { healthcheck };
